96fa07e54a40550188a6986cd3777aeac53bf971
Lucas Mezencio <lucasmezencio@outlook.com>
2021-08-09 18:19:54 -0300

    implemente delete method on notes contendt provider

--- app/src/main/java/com/lucasmezencio/applicationcontentprovider/database/NotesProvider.kt
+++ app/src/main/java/com/lucasmezencio/applicationcontentprovider/database/NotesProvider.kt
@@ -2,33 +2,43 @@ package com.lucasmezencio.applicationcontentprovider.database
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.media.UnsupportedSchemeException
 import android.net.Uri
+import com.lucasmezencio.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TABLE_NOTES
+import android.provider.BaseColumns._ID
 
 class NotesProvider : ContentProvider() {
 
     private lateinit var mUriMatcher: UriMatcher
+    private lateinit var dbHelper: NotesDatabaseHelper
+
     // inicializa tudo no cont prov: inicializa um db, por exemplo
     override fun onCreate(): Boolean {
         mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
         mUriMatcher.addURI(AUTHORITY, "notes", NOTES)
         mUriMatcher.addURI(AUTHORITY, "notes/#", NOTES_BY_ID)
+        if (context != null) { dbHelper = NotesDatabaseHelper(context as Context) }
         return true
     }
 
-    // Deleta dados do provider
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
+        if (mUriMatcher.match(uri) == NOTES_BY_ID) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffected = db.delete(TABLE_NOTES, "$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffected
+        } else {
+            throw UnsupportedSchemeException("URI invalida para exclusao!")
+        }
     }
 
     // valida uma url
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
-    }
+    override fun getType(uri: Uri): String? = throw UnsupportedSchemeException("URI no implementada!")
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
         TODO("Implement this to handle requests to insert a new row.")
